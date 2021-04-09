4e41e127bc8c74e99f0c32db843d51d4d8c0d841
Anisa S <Anisa.Sulistyaningsih16@gmail.com>
2021-04-09 10:25:01 +0700

    soal 1

---
+++ I0320010_soal 1_tugas 6.py
@@ -0,0 +1,9 @@
+#Mencetak teks "Hello Worl" sebanyak 10 kali dengan while
+#Mengunakan tipe angka
+kata = 1
+#pengulangan sebanyak 10
+n = 10
+while kata <= n:
+    print('Hello World!')
+    kata += 1
+
